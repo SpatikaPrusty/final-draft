278e2b2bb2a2a4be5db0a7fd6313e24224337a54
SpatikaPrusty <kspatika.prusty@lumiq.ai>
2024-02-26 18:15:30 +0530

    docker

--- Backend/database/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require( "mongoose");
-
-async function connect(){
-    await mongoose.connect(process.env.ATLAS_URI)
-    console.log("Database Connected")
-}
-
-module.exports = connect;
